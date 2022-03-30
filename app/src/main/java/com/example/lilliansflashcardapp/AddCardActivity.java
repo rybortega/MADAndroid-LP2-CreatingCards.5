4ed2349355df7345b8b883ba6832f78a851c6db4
LillianHo5 <lmho@ucsd.edu>
2022-03-29 21:20:51 -0700

    Lab 2 Optional Activities

--- app/src/main/java/com/example/lilliansflashcardapp/AddCardActivity.java
+++ app/src/main/java/com/example/lilliansflashcardapp/AddCardActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
     EditText question;
@@ -17,12 +19,22 @@ public class AddCardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
-        String currentQuestion = getIntent().getStringExtra("question edit");
-        String currentAnswer1 = getIntent().getStringExtra("answer 1 edit");
-        String currentAnswer2 = getIntent().getStringExtra("answer 1 edit");
-        String currentAnswer3 = getIntent().getStringExtra("answer 1 edit");
 
-
+        question = findViewById(R.id.question_text);
+        answer1 = findViewById(R.id.answer_text1);
+        answer2 = findViewById(R.id.answer_text2);
+        answer3 = findViewById(R.id.answer_text3);
+        Intent currIntent = getIntent();
+        if(currIntent != null) {
+            String currentQuestion = getIntent().getStringExtra("question edit");
+            String currentAnswer1 = getIntent().getStringExtra("answer 1 edit");
+            String currentAnswer2 = getIntent().getStringExtra("answer 2 edit");
+            String currentAnswer3 = getIntent().getStringExtra("answer 3 edit");
+            question.setText(currentQuestion);
+            answer1.setText(currentAnswer1);
+            answer2.setText(currentAnswer2);
+            answer3.setText(currentAnswer3);
+        }
 
         findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +57,15 @@ public class AddCardActivity extends AppCompatActivity {
                 data.putExtra("answer2", answerText2);
                 data.putExtra("answer3", answerText3);
                 setResult(RESULT_OK, data);
-                finish();
+                //Displays an error message if not all EditText views are populated
+                if(questionText.isEmpty() || answerText1.isEmpty() || answerText2.isEmpty() || answerText3.isEmpty()) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Must enter all questions and answers!", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    toast.show();
+                } else {
+                    System.out.println("we pass by here");
+                    finish();
+                }
             }
         });
     }
--- app/src/main/java/com/example/lilliansflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/lilliansflashcardapp/MainActivity.java
@@ -10,21 +10,39 @@ import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import org.w3c.dom.Text;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     TextView question;
     TextView answer1;
     TextView answer2;
     TextView answer3;
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
         TextView flashcardQuestion = findViewById(R.id.flashcard_question);
         TextView flashcardAnswer1 = findViewById(R.id.flashcard_answer1);
         TextView flashcardAnswer2 = findViewById(R.id.flashcard_answer2);
         TextView flashcardAnswer3 = findViewById(R.id.flashcard_answer3);
+
+        flashcardDatabase = new FlashcardDatabase(this);
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            flashcardQuestion.setText(allFlashcards.get(0).getQuestion());
+            flashcardAnswer1.setText(allFlashcards.get(0).getWrongAnswer1());
+            flashcardAnswer2.setText(allFlashcards.get(0).getWrongAnswer2());
+            flashcardAnswer3.setText(allFlashcards.get(0).getAnswer());
+        }
 
         flashcardAnswer1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,13 +88,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (isShowingAnswers[0]) {
-                    ((ImageView) toggleVisibility).setImageResource(R.drawable.do_not_see_icon_foreground);
+                    toggleVisibility.setImageResource(R.drawable.do_not_see_icon_foreground);
                     flashcardAnswer1.setVisibility(View.VISIBLE);
                     flashcardAnswer2.setVisibility(View.VISIBLE);
                     flashcardAnswer3.setVisibility(View.VISIBLE);
                     isShowingAnswers[0] = false;
                 } else {
-                    ((ImageView) toggleVisibility).setImageResource(R.drawable.see_icon_foreground);
+                    toggleVisibility.setImageResource(R.drawable.see_icon_foreground);
                     flashcardAnswer1.setVisibility(View.INVISIBLE);
                     flashcardAnswer2.setVisibility(View.INVISIBLE);
                     flashcardAnswer3.setVisibility(View.INVISIBLE);
@@ -94,19 +112,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //When users tap on the edit button for the flashcard
         findViewById(R.id.edit_button).setOnClickListener(new View.OnClickListener() {
-            String questionString = flashcardQuestion.getText().toString();
-            String flashcardAnswer1String = flashcardAnswer1.getText().toString();
-            String flashcardAnswer2String = flashcardAnswer2.getText().toString();
-            String flashcardAnswer3String = flashcardAnswer3.getText().toString();
+
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                intent.putExtra("question edit", questionString);
-                intent.putExtra("answer 1 edit", flashcardAnswer1String);
-                intent.putExtra("answer 2 edit", flashcardAnswer2String);
-                intent.putExtra("answer 3 edit", flashcardAnswer3String);
+                intent.putExtra("question edit", flashcardQuestion.getText().toString());
+                intent.putExtra("answer 1 edit", flashcardAnswer1.getText().toString());
+                intent.putExtra("answer 2 edit", flashcardAnswer2.getText().toString());
+                intent.putExtra("answer 3 edit", flashcardAnswer3.getText().toString());
+                System.out.println("we passed by here me thinks");
                 MainActivity.this.startActivityForResult(intent, 200);
+            }
+        });
+
+
+        //When users tap on the next button on the app
+        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(allFlashcards.size() == 0) {
+                    return;
+                }
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if(currentCardDisplayedIndex >= allFlashcards.size()) {
+                    Snackbar.make(view,
+                            "You've reached the end of the cards, going back to start.",
+                            Snackbar.LENGTH_SHORT)
+                            .show();
+                    currentCardDisplayedIndex = 0;
+                }
+                // set the question and answer TextViews with data from the database
+                allFlashcards = flashcardDatabase.getAllCards();
+                Flashcard flashcard = allFlashcards.get(currentCardDisplayedIndex);
+
+                flashcardQuestion.setText(flashcard.getQuestion());
+                flashcardAnswer1.setText(flashcard.getWrongAnswer1());
+                flashcardAnswer2.setText(flashcard.getWrongAnswer2());
+                flashcardAnswer3.setText(flashcard.getAnswer());
             }
         });
     }
@@ -126,10 +172,35 @@ public class MainActivity extends AppCompatActivity {
                 String answer1String = data.getExtras().getString("answer1");
                 String answer2String = data.getExtras().getString("answer2");
                 String answer3String = data.getExtras().getString("answer3");
+
                 question.setText(questionString);
                 answer1.setText(answer1String);
                 answer2.setText(answer2String);
                 answer3.setText(answer3String);
+
+                //save flashcard data into database
+                flashcardDatabase.insertCard(new Flashcard(questionString, answer1String,
+                        answer2String, answer3String));
+                allFlashcards = flashcardDatabase.getAllCards();
+                System.out.println("add logic used here!");
+                Snackbar.make(findViewById(R.id.flashcard_question),
+                        "Card successfully created.", Snackbar.LENGTH_SHORT).show();
+            }
+        }
+        if(requestCode == 200) {
+            if (data != null) {
+                String questionString = data.getExtras().getString("question");
+                String answer1String = data.getExtras().getString("answer1");
+                String answer2String = data.getExtras().getString("answer2");
+                String answer3String = data.getExtras().getString("answer3");
+
+                question.setText(questionString);
+                answer1.setText(answer1String);
+                answer2.setText(answer2String);
+                answer3.setText(answer3String);
+
+                Snackbar.make(findViewById(R.id.flashcard_question),
+                        "Card successfully edited.", Snackbar.LENGTH_SHORT).show();
             }
         }
     }
